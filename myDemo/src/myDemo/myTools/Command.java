0b3f291c3282e90407cf725bcb3350c12317efbf
睡不醒的猫头鹰 <844910815@qq.com>
2017-03-29 14:10:05 +0800

    模拟cmd 控制台的操作,输入指令执行特定的方法

---
+++ myDemo/src/myDemo/myTools/Command.java
@@ -0,0 +1,9 @@
+package myDemo.myTools;
+
+public interface Command {
+	/**
+	 * 结束命令
+	 */
+	@Description(commandType="sys",description="结束退出命令")
+	public static final String END="END";
+}
---
+++ myDemo/src/myDemo/myTools/MainClass.java
@@ -0,0 +1,7 @@
+package myDemo.myTools;
+
+public class MainClass {
+	public static void main(String[] args) {
+		
+	}
+}
