d0fbdde2c2eb5f60a9af54a74beb87cabac130e6
睡不醒的猫头鹰 <844910815@qq.com>
2017-04-05 02:18:31 +0800

    新的测试方法,测试适配器模式 一个target 类的接口中 , 定义了一个方法1 和一个方法2 的抽象方法,在source 类中 只有方法1, 使用适配器 继承 source 实现 target  这样的话 获取的targettable 的实例中就有target1和 target2了

--- myDemo/src/Tools/TestClass.java
+++ myDemo/src/Tools/TestClass.java
@@ -1,6 +1,13 @@
 package Tools;
 
+import myDemo.desingModel.adapterModel.Adapter;
+import myDemo.desingModel.adapterModel.TargetTable;
+
 public class TestClass {
-	
+	public static void main(String[] args) {
+		TargetTable targetTable = new Adapter();
+		targetTable.method1();
+		targetTable.method2();
+	}
 	
 }
